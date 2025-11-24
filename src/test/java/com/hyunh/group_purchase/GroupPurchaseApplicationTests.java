2885e5ba66c52b6db8a2823736035eb2f5969eee
BB545 <black1016@hanmail.net>
2025-11-24 17:31:45 +0900

    [create] spring project 초기 생성

---
+++ src/test/java/com/hyunh/group_purchase/GroupPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunh.group_purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
