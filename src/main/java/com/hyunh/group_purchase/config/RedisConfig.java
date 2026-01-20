2f7ac6b0121bfb69eb36a9a26d3ae01d17a9b80a
BB545 <black1016@hanmail.net>
2026-01-21 02:10:23 +0900

    feat/#3: Redis, Kafka 설정 및 테스트 완료
     - 변경 내용
     1. Redis 연결 - RedisConfig 작성
     2. Kafka 연결 - KafkaConfig, KafkaProducerConfig, KafkaConsumerConfig 작성
     3. docker-compose.yml - advertised listeners를 2개로 설정 (내부 + 외부)

---
+++ src/main/java/com/hyunh/group_purchase/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.hyunh.group_purchase.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration // 설정 파일
+public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory());
+
+        template.setKeySerializer(new StringRedisSerializer()); // key = 문자열
+        template.setValueSerializer(new StringRedisSerializer()); // value = 문자열
+
+        return template;
+    }
+}
